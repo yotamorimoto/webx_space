d816cd20347fca568d80cdebf9f4eb986539101a
Yota Morimoto <yota@dark.local>
2020-09-04 12:31:41 +0200

    camera default

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ var controls;
 
 const gl = document.getElementById('gl');
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000);
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 0;
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100);
+// camera.position.x = 0;
+// camera.position.y = 0;
+// camera.position.z = 0;
 // camera.rotation.reorder('YXZ');
 
 // --------- renderer
