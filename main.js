afb994f1fff9600448c9724b3e6bf41c7a784f77
Yota Morimoto <yota@dark.local>
2020-09-17 19:59:05 +0200

    js

--- main.js
+++ main.js
@@ -166,7 +166,8 @@ function loop(){
 	// rotator.pitch = camera.rotation.x*180/Math.PI;
 	// rotator.roll = camera.rotation.z*180/Math.PI;
 	// rotator.updateRotMtx();
-	// renderer.render(scene, camera);
+	// ----------------------------
+	renderer.render(scene, camera);
 };
 
 function askFullscreen() {
